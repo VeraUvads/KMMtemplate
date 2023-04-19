7c4282e6b0ad39b7e4f94fc1dd8eb2a42e97af0a
vera <vero971410@gmail.com>
2023-04-19 19:58:05 +0200

    added dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -39,11 +39,12 @@ android {
 }
 
 dependencies {
+    val composeVersion = "1.4.1"
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.4.0")
-    implementation("androidx.compose.ui:ui-tooling:1.4.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0")
-    implementation("androidx.compose.foundation:foundation:1.4.0")
-    implementation("androidx.compose.material:material:1.4.0")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.activity:activity-compose:1.7.0")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,15 +15,19 @@ dependencyResolutionManagement {
         create("libs") {
             library("kotlinx-coroutines-core", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
             library("touchlab-kermit", "co.touchlab:kermit:1.2.2")
-            library("ktor-client-core", "io.ktor:ktor-client-core:2.2.4")
-            library("ktor-client-ios", "io.ktor:ktor-client-ios:2.2.4")
-            library("ktor-client-okhttp", "io.ktor:ktor-client-okhttp:2.2.4")
-            library("sqldelight-driver-native", "com.squareup.sqldelight:native-driver:1.5.5")
-            library("sqldelight-driver-android", "com.squareup.sqldelight:android-driver:1.5.5")
+
+            val ktorVersion = "2.2.4"
+            library("ktor-client-core", "io.ktor:ktor-client-core:$ktorVersion")
+            library("ktor-client-ios", "io.ktor:ktor-client-ios:$ktorVersion")
+            library("ktor-client-okhttp", "io.ktor:ktor-client-okhttp:$ktorVersion")
+
+            val sqlDelVersion = "1.5.5"
+            library("sqldelight-driver-native", "com.squareup.sqldelight:native-driver:$sqlDelVersion")
+            library("sqldelight-driver-android", "com.squareup.sqldelight:android-driver:$sqlDelVersion")
         }
     }
 }
 
-rootProject.name = "KMM_template"
+rootProject.name = "KMM template"
 include(":androidApp")
 include(":shared")
